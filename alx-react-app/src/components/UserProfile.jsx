0c93bd33d0f3e24d2eb33b616b25823b087cbba6
okoli peace <okolipeacekaycee@gmail.com>
2025-03-24 13:18:22 +0100

    commiting errors

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 function UserProfile(props) {
     return(
         <div>
+            {/* ["Age", "Bio", Name] */}
             <h2>{props.name}</h2>
-            <p> {props.age} </p>
-            <p> {props.bio}</p>
+            <p> Age: {props.age} </p>
+            <p> Bio: {props.bio}</p>
         </div>
     );
 };
