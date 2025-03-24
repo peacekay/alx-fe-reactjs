26425d38271a1a1073bfcb371b490ea37617f5f6
okoli peace <okolipeacekaycee@gmail.com>
2025-03-24 13:06:42 +0100

    commiting corrections

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
@@ -14,11 +14,12 @@ function App() {
   return (
     <>
       <div>
+        {/* rendering ["<Header />", "<Footer />", "<MainContent />"]components */}
         <Header></Header>
         <MainContent></MainContent>
         <Footer></Footer>
         <WelcomeMessage />
-        <UserProfile name="Peace"age="32"bio="Is a frontend Developer at ALX" />
+        <UserProfile Name="Peace"Age="32"Bio="Is a frontend Developer at ALX" />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
