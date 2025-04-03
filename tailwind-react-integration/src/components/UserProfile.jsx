2bb0e825ae38b5709abece7d2e8eabaf726992b6
okoli peace <okolipeacekaycee@gmail.com>
2025-04-03 11:47:03 +0100

    commiting errors

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p4 sm:p-8 max-w-xs sm: mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl hover:scale-110 transition-transform duration-300 ease-in-out md:p-8 sm:p-4 max-w-sm user-profile">
+    <div className="bg-gray-100 p4 sm:p-8 max-w-xs sm: mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl hover:scale-110 transition-transform duration-300 ease-in-out md:p-8 sm:p-4 max-w-sm h-36 md:max-w-sm md:text-xl user-profile">
       <img
         className="rounded-full w24 h24 sm:w-36 sm:h36 mx-auto"
         src="https://via.placeholder.com/150"
